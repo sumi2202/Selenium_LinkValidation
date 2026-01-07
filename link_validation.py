971905c7a0895366307d4fa55c73e87c12ea657d
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2026-01-07 15:02:00 -0500

    add url input

--- link_validation.py
+++ link_validation.py
@@ -7,3 +7,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 import requests
 import csv
 
+options = Options()
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=options
+)
+
+url = input("Enter the URL to check for broken links: ").strip()
+driver.get(url)
